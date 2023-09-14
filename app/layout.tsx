e788972149a429692c84b5255406a0c507624301
Flavio <fjcf@poli.br>
2023-09-14 18:13:08 -0300

    feat: add contact component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
 import { Header } from './components/Header'
+import { ContactForm } from './components/ContactForm'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <Header />
         {children}
+        <ContactForm />
       </body>
     </html>
   )
