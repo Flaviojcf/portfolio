e9b225211af75851d1bd27c67ce12575df347363
Flavio <fjcf@poli.br>
2023-09-17 18:12:24 -0300

    feat: add function to hygraph query

---
+++ app/utils/fetchHygraphQuery.ts
@@ -0,0 +1,20 @@
+export const fetchHygraphQuery = async (query: string) => {
+  const response = await fetch(process.env.HYGRAPH_URL!, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query,
+      next: {
+        revalidate: 60 * 60 * 24,
+      },
+    }),
+  })
+
+  const { data } = await response.json()
+
+  return data
+}
