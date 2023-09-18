9ec976c1f6e8cdd20581d82906602fc9bd50e73a
Flavio <fjcf@poli.br>
2023-09-18 19:08:59 -0300

    feat: add hygraph requisition

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,43 @@ import { MyProjectsSection } from './components/Pages/Home/MyProjectsSection'
 import { ProfileSection } from './components/Pages/Home/ProfileSection'
 import { TechsSection } from './components/Pages/Home/TechsSection'
 import { WorkExperienceSection } from './components/Pages/Home/WorkExperienceSection'
+import { HomePageData } from './types/pageInfo'
+import { fetchHygraphQuery } from './utils/fetchHygraphQuery'
+
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+  return fetchHygraphQuery(query)
+}
 
 export default async function Home() {
+  const { page: pageData } = await getPageData()
+
   return (
     <>
-      <ProfileSection />
+      <ProfileSection homeInfo={pageData} />
       <TechsSection />
       <MyProjectsSection />
       <WorkExperienceSection />
