c4f7d65b4e93e5f10b659965ff390ea746b828ea
Flavio <fjcf@poli.br>
2023-09-18 18:58:47 -0300

    refactor: change revalidate time

--- app/utils/fetchHygraphQuery.ts
+++ app/utils/fetchHygraphQuery.ts
@@ -9,7 +9,7 @@ export const fetchHygraphQuery = async (query: string) => {
     body: JSON.stringify({
       query,
       next: {
-        revalidate: 60 * 60 * 24,
+        revalidate: 60 * 1,
       },
     }),
   })
